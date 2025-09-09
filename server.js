5372390c2342f3782a40997d95db369547f877ba
Prahalad-2004 <prahaladpal313@gmail.com>
2025-09-09 10:15:17 +0530

    Fix route order to prevent path-to-regexp error

--- server.js
+++ server.js
@@ -36,13 +36,13 @@ const __dirname = path.dirname(__filename);
  app.use('/api/v1/product',productRoutes)
  app.use("/api/v1/payment", paymentRoutes);
 
- // Serve static files from the React app build directory
- app.use(express.static(path.join(__dirname, 'client/dist')));
-
  // API route
  app.get('/api', (req,res) => {
     res.send("<h1>Welcome to E-commerce API - Auto-deploy test</h1>");
  });
+
+ // Serve static files from the React app build directory
+ app.use(express.static(path.join(__dirname, 'client/dist')));
 
  // Catch all handler: send back React's index.html file for any non-API routes
  app.get('*', (req, res) => {
