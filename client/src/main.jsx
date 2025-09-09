5cfbca7b0d02582735a02a71a79b8ccf44f72eb9
Prahalad-2004 <prahaladpal313@gmail.com>
2025-09-09 09:45:33 +0530

    Fix API_BASE_URL import - use default export instead of named export

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './context/auth.jsx';
 import { SearchProvider } from './context/search.jsx'
 import { CartProvider } from './context/cart.jsx'
 import axios from 'axios'
-import { API_BASE_URL } from './config/api.js'
+import API_BASE_URL from './config/api.js'
 
 // Configure axios base URL
 axios.defaults.baseURL = API_BASE_URL;
