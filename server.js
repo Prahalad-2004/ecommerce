2d1df38e60d1660fa545d310ac66edcab20c0853
Prahalad Pal <prahaladpal313@gmail.com>
2025-09-15 00:21:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const __dirname = path.dirname(__filename);
  const app =express()
 
  //middlewares
- app.use(cors())
+ app.use(cors({ origin: process.env.CLIENT_URL }));
  app.use(express.json())
  app.use(morgan('dev'))
 
